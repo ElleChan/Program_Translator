5a7a267ddbf13fcfd74f544bfa7c5a1afecd1f8d
Justin Hale <justinhale345@gmail.com>
2019-10-09 13:39:41 -0600

    Create JSON decoder file

---
+++ decoder.py
@@ -0,0 +1,12 @@
+import json
+from os.path import join, dirname, realpath
+
+def decoder(path):
+    with open(path) as ifile:
+        print(ifile)
+        return json.load(ifile)
+
+if __name__ == '__main__':
+    home = dirname(realpath(__file__))
+    results = decoder(join(home, 'tree2tree_data', 'java2c#', 'antlr-data.json'))
+    print(results)
