5b9abb1fdaf49c1afc6dda7b9c8740073654f0e6
Justin Hale <justinhale345@gmail.com>
2019-10-14 08:56:44 -0600

    Create JSON decoder file

--- decoder.py
+++ decoder.py
@@ -1,13 +1,20 @@
 import json
+from collections import namedtuple
 from os.path import join, dirname, realpath
+
+Trees = namedtuple('Trees', 'java cs')
+
 
 def decoder(path):
     with open(path) as ifile:
-        print(ifile)
-        return json.load(ifile)
+        return [Trees(x['java_ast'], x['cs_ast']) for x in json.load(ifile)]
+
 
 
 if __name__ == '__main__':
     home = dirname(realpath(__file__))
     results = decoder(join(home, 'tree2tree_data', 'java2c#', 'antlr-data.json'))
-    print(results)
+    for value in results:
+        java, cs = value
+        print(java)
+        print(cs)
