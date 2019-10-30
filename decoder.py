df9611d1a9eaa1ccf47888378c1370b050955442
ElleChan <programmer.jael.butler@gmail.com>
2019-10-29 19:45:30 -0600

    Changed decoder

--- decoder.py
+++ decoder.py
@@ -5,6 +5,7 @@ from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 
 
 # Reference: https://github.com/IBM/pytorch-seq2seq/
+#            https://pytorch.org/tutorials/intermediate/seq2seq_translation_tutorial.html
 class DecoderModel(nn.Module):
     def __init__(self, output_size, hidden_size):
         super(DecoderModel, self).__init__()
@@ -17,11 +18,12 @@ class DecoderModel(nn.Module):
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden, shape):
-        embedded = pack_padded_sequence(input, shape, batch_first=True, enforce_sorted=False)
-        output = F.relu(input)
-        output, hidden = self.gru(output, hidden)
-        embedded = pad_packed_sequence(embedded, batch_first=True)
-        output = self.softmax(self.out(embedded[0]))
+        embedded = self.embedding(input)
+        embedded = pack_padded_sequence(embedded, shape, batch_first=True, enforce_sorted=False)
+        embedded = F.relu(embedded)
+        output, hidden = self.gru(embedded, hidden)
+        output = pad_packed_sequence(output, batch_first=True)
+        output = self.softmax(self.out(output[0]))
         return output, hidden
 
     def initHidden(self):
