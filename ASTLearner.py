ba1141231548e87125f926561329db6051a3291c
Justin Hale <jhale2@cs-gpu2.uccs.edu>
2019-12-18 14:06:50 -0700

    Merge with master

--- ASTLearner.py
+++ ASTLearner.py
@@ -2,68 +2,55 @@ from languages import ASTNumbering
 import numpy as np
 from numpy.random import choice
 from decoder import DecoderModel
-from encoder import EncoderModel
+from encoder import EncoderModel, TransformerModel
 import torch
 
 class ASTModel:
     def __init__(self, java_language, cs_language, loss_func=torch.nn.NLLLoss()):
         self.java_language = java_language
         self.cs_language = cs_language
-<<<<<<< HEAD
-        self.encoder = EncoderModel(java_language.max_length, java_language.max_length, 1)
-        self.decoder = DecoderModel(java_language.max_length, java_language.max_length)
-=======
         #self.encoder = EncoderModel(java_language.max_length, java_language.max_length, 1)
         #self.decoder = DecoderModel(java_language.max_length, java_language.max_length, 1)
->>>>>>> fdf3d950c5559954a2f5c386e258e16f92c2497a
         self.loss = loss_func
         #self._hidden_encoder = self.encoder.initialize_hidden()
         #self._hidden_decoder = 0
         
-    def train(self, train_data, epochs, batch_size):
-        train_data = [self._prepare_data(x) for x in train_data]
+    def train(self, train_data, epochs, batch_size, verbose=False):
+        #train_data = [self._prepare_data(x) for x in train_data]
         java_train = []
         cs_train = []
         
         for i in train_data:
-            java_train.append(i[0])
-            cs_train.append(i[1])
+            java_train.append(i['java_ast'])
+            cs_train.append(i['cs_ast'])
+
+        self.fit(epochs, java_train, cs_train, batch_size, 0.1, verbose)
+
+    def fit(self, epochs, java_train, cs_train, batch_size, learning_rate, verbose):
         for _ in range(epochs):
-            matrix, lengths = self._create_batch(java_train, batch_size)
+            matrix, _, lengths = self._create_batch(java_train, batch_size)
             print(matrix, lengths, sep='\n\n')
             outputs = []
             o, self._hidden_encoder = self.encoder.forward(matrix, self._hidden_encoder, lengths)
             print(o)
 
-
     def predict(self, data):
         pass
         
-    def _create_batch(self, data, batch_size):
-        vectors = choice(len(data), size=batch_size)
-        matrix = torch.zeros(batch_size, self.java_language.max_length)
-        lengths = torch.zeros(batch_size)
-        counter = 0
-        for i in vectors:
-            print(data[i][0].shape)
-            
-            matrix[counter, :] = data[i][0][0, :]
-            lengths[counter] = data[i][1]
-            counter += 1
-        return matrix, lengths
+    def _create_batch(self, x, batch_size):
+        vectors = choice(len(x), size=batch_size)
+        return vectors
 
     def _prepare_data(self, data):
         return (self.java_language.create_vector(data['java_ast']),
                  self.cs_language.create_vector(data['cs_ast']))
-<<<<<<< HEAD
-=======
 
     def eval(self, x, y):
         pass
 
 
 class ASTModelTransformer(ASTModel):
-    def __init__(self, java_language, cs_language, loss_func=torch.nn.NLLLoss(), heads=1, dim_feedforward=2048,
+    def __init__(self, java_language, cs_language, loss_func=torch.nn.Cross(), heads=1, dim_feedforward=2048,
                  dropout=0.1, activation='relu', number_encoder_layers=1, number_decoder_layers=1):
         print("Creating Transformer Model")
         super().__init__(java_language, cs_language, loss_func)
@@ -154,4 +141,3 @@ class ASTModelTransformer(ASTModel):
             output = output.view(-1, self.java_language.max_length)
             total_loss += len(vector) * self.loss(output, y)
         return total_loss / (len(x) - 1)
->>>>>>> fdf3d950c5559954a2f5c386e258e16f92c2497a
